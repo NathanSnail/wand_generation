90a53c19f737025a247f5317ae32687f82e12e62
Nathan <nathan.titirangi@gmail.com>
2025-07-14 18:43:05 +1200

    __mul sequences

--- files/lib/gen.lua
+++ files/lib/gen.lua
@@ -7,15 +7,15 @@ local M = {}
 ---@operator mul(gen.Generator): gen.SequenceGenerator
 
 ---@class gen.TerminalGenerator: gen.Generator
----@field private terminal any
+---@field terminal any
 
 ---@class gen.SequenceGenerator: gen.Generator
----@field private left gen.Generator
----@field private right gen.Generator
+---@field left gen.Generator
+---@field right gen.Generator
 
 ---@class gen.ParameterisedGenerator
----@field private generator gen.Generator
----@field private weight number
+---@field generator gen.Generator
+---@field weight number
 
 ---@param dest table
 ---@param source table
@@ -27,7 +27,7 @@ local function merge(dest, source)
 	return dest
 end
 
-local parameterised_generator_mt, generator_mt
+local parameterised_generator_mt, generator_mt, sequence_mt
 
 generator_mt = {
 	---@param self gen.Generator
@@ -41,17 +41,35 @@ generator_mt = {
 	---@param other gen.Generator
 	---@return gen.SequenceGenerator
 	__mul = function(self, other)
-		return {}
+		return setmetatable({ left = self, right = other }, sequence_mt)
 	end,
 }
 
 local terminal_mt = merge({
 	__index = {
 		---@param self gen.TerminalGenerator
-		---@return unknown
+		---@return any
 		generate = function(self)
-			---@diagnostic disable-next-line: invisible
-			return self.terminal
+			return { self.terminal }
+		end,
+	},
+}, generator_mt)
+
+sequence_mt = merge({
+	__index = {
+		---@param self gen.SequenceGenerator
+		---@return any
+		generate = function(self)
+			local output = {}
+
+			for _, v in ipairs(self.left:generate()) do
+				table.insert(output, v)
+			end
+			for _, v in ipairs(self.right:generate()) do
+				table.insert(output, v)
+			end
+
+			return output
 		end,
 	},
 }, generator_mt)
--- files/test/generator_test.lua
+++ files/test/generator_test.lua
@@ -1,13 +1,30 @@
 local test = require("files.test.test")
 -- test sets the TESTING global so it goes first
 
-local generators = require("files.lib.generators")
+local gen = require("files.lib.gen")
 
 test.test({
 	{
 		name = "Trivial terminal",
 		body = function()
-			test.eq(generators.terminal("test"):generate(), "test")
+			test.eq(gen.terminal("test"):generate(), { "test" })
+		end,
+	},
+	{
+		name = "Sequence",
+		body = function()
+			---@type gen.SequenceGenerator
+			local sequence = gen.terminal(1) * gen.terminal(2)
+			test.eq(sequence:generate(), { 1, 2 })
+		end,
+	},
+	{
+		name = "Long Sequence",
+		body = function()
+			local one = gen.terminal(1)
+			local two = gen.terminal(2)
+			local sequence = one * two * one * two
+			test.eq(sequence:generate(), { 1, 2, 1, 2 })
 		end,
 	},
 })
